b34d16048678c6cf67d8b420cd603147501da6e8
Saurabh <36810360+iamsdp@users.noreply.github.com>
2020-04-08 18:39:15 +0530

    Create readOutlookMsgFile.py

---
+++ readOutlookMsgFile.py
@@ -0,0 +1,16 @@
+import extract_msg
+
+p = 'C:/Users/sp250123/Desktop/CBA Python/scheduled autosupport (BARBWDD1.localdomain) .msg'
+f = r'C:/Users/sp250123/Desktop/CBA Python/scheduled autosupport (BARBWDD1.localdomain) .msg'  # Replace with yours
+
+msg = extract_msg.Message(f)
+msg_sender = msg.sender
+msg_date = msg.date
+msg_subj = msg.subject
+msg_message = msg.body
+
+print('Sender: {}'.format(msg_sender))
+print('Sent On: {}'.format(msg_date))
+print('Subject: {}'.format(msg_subj))
+print('Body: {}'.format(msg_message))
+print (msg_message)
