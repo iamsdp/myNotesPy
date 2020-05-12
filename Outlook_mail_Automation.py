e9680d0c15080de8031cdb5953d19678ca4c5042
Saurabh <36810360+iamsdp@users.noreply.github.com>
2020-05-12 19:39:32 +0530

    Create Outlook_mail_Automation.py

---
+++ Outlook_mail_Automation.py
@@ -0,0 +1,17 @@
+# link : https://stackoverflow.com/questions/20956424/how-do-i-generate-and-open-an-outlook-email-with-python-but-do-not-send
+# This mail generate draft :-> using mail.save() method. 
+
+def Emailer(text, subject, recipient):
+    import win32com.client as win32
+
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = recipient
+    mail.Subject = subject
+    mail.HtmlBody = text
+    mail.save()
+
+text = "<h1>Zoke!</h1>"   #You can pass HTML code here
+Emailer(text,'lets see','Saurabh.Patil@Teradata.com')
+
+# try : mail.Display(True)
